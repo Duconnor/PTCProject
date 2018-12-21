165dee895b347ed9a66c0275cfc922aa9bf75540
Duconnor <duconnor@outlook.com>
2018-12-21 16:21:57 +0800

    parser and tm finish

--- src/TM.java
+++ src/TM.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class TM {
     private static final String blank = "_";
@@ -61,9 +62,10 @@ public class TM {
 
     /* vars that change every time after TM moves */
     private String currentState;
-    private String[] tape;
+    private LinkedList<String> tape;
     private int step;
     private int headIndex; // the index of the read-write head
+    private int startIndex; // the index of the start point
 
     TM() {
         stateSet = new HashSet<>();
@@ -72,40 +74,40 @@ public class TM {
         finalStateSet = new HashSet<>();
         transitionFunction = new HashMap<>();
         currentState = null;
-        tape = null;
-        step = -1;
-        headIndex = -1;
+        tape = new LinkedList<>();
+        step = 0;
+        headIndex = startIndex = 0;
     }
 
-    public void addState(String state) {
+    private void addState(String state) {
         stateSet.add(state);
     }
 
-    public void addInputSymbol(String inputSymbol) {
+    private void addInputSymbol(String inputSymbol) {
         inputSymbolSet.add(inputSymbol);
     }
 
-    public void addTapeSymbol(String tapeSymbol) {
+    private void addTapeSymbol(String tapeSymbol) {
         tapeSymbolSet.add(tapeSymbol);
     }
 
-    public void setInitialState(String initialState) {
+    private void setInitialState(String initialState) {
         this.initialState = initialState;
     }
 
-    public void addFinalState(String finalState) {
+    private void addFinalState(String finalState) {
         finalStateSet.add(finalState);
     }
 
-    public void addTransitionFunction(String currentState, String currentSymbol,
-                                      String newSymbol, String directionStr, String newState) {
+    private void addTransitionFunction(String currentState, String currentSymbol,
+                                       String newSymbol, String directionStr, String newState) {
         TransitionInput transitionInput = new TransitionInput(currentState, currentSymbol);
         Direction direction = null;
-        if (directionStr.compareTo("L") == 0) {
+        if (directionStr.compareTo("l") == 0) {
             direction = Direction.L;
-        } else if (directionStr.compareTo("R") == 0) {
+        } else if (directionStr.compareTo("r") == 0) {
             direction = Direction.R;
-        } else if (directionStr.compareTo("N") == 0) {
+        } else if (directionStr.compareTo("*") == 0) {
             direction = Direction.N;
         } else {
             System.out.println("Unknown direction");
@@ -113,5 +115,170 @@ public class TM {
         }
         TransitionOutput transitionOutput = new TransitionOutput(newState, newSymbol, direction);
         transitionFunction.put(transitionInput, transitionOutput);
+    }
+
+    private int findFirstNonBlank() {
+        // return the index of the first non-blank symbol or the headIndex
+        for (int i = 0; i < headIndex; i++) {
+            if (tape.get(i).compareTo(blank) != 0)
+                return i;
+        }
+        return headIndex;
+    }
+
+    private int findLastNonBlank() {
+        // return the index of the last non-blank symbol or the headIndex
+        for (int i = tape.size() - 1; i > headIndex; i--) {
+            if (tape.get(i).compareTo(blank) != 0)
+                return i;
+        }
+        return headIndex;
+    }
+
+    private int howManyDigits(int number) {
+        // how many digits in this number
+        int digit = 1;
+        while (number / (int) Math.pow(10, digit) != 0)
+            digit++;
+        return digit;
+    }
+
+    private void printSpace(int number) {
+        // print number spaces on console
+        for (int i = 0; i < number; i++)
+            System.out.print(" ");
+    }
+
+    private void printID() {
+        System.out.println("Step:  " + step);
+        System.out.print("Index: ");
+        int first = findFirstNonBlank();
+        int last = findLastNonBlank();
+        for (int i = first; i <= last; i++) {
+            if (i != tape.size() - 1)
+                System.out.print(Math.abs(i - startIndex) + " ");
+            else
+                System.out.print(Math.abs(i - startIndex));
+        }
+        System.out.println();
+        System.out.print("Tape:  ");
+        for (int i = first; i <= last; i++) {
+            String symbol = tape.get(i);
+            if (i != tape.size() - 1) {
+                System.out.print(symbol);
+                printSpace(howManyDigits(i));
+            } else
+                System.out.print(symbol);
+        }
+        System.out.println();
+        System.out.print("Head:  ");
+        for (int i = first; i <= last; i++) {
+            if (i == headIndex) {
+                System.out.println("^");
+                break;
+            } else {
+                printSpace(howManyDigits(i) + 1);
+            }
+        }
+        System.out.println("State: " + currentState);
+    }
+
+    private boolean oneMove() {
+        // make one move
+        // first, find transition function
+        step++;
+        String currentSymbol = tape.get(headIndex);
+        TransitionInput input = new TransitionInput(currentState, currentSymbol);
+        TransitionOutput output = transitionFunction.get(input);
+        if (output == null) {
+            // try wildcard
+            input = new TransitionInput(currentState, "*");
+            output = transitionFunction.get(input);
+            if (output == null)
+                return false; // can't move further
+        }
+        // second, move accordingly
+        currentState = output.nextState; // change state
+        if (output.newSymbol.compareTo("*") != 0)
+            tape.set(headIndex, output.newSymbol); // write symbol on tape
+        if (output.direction == Direction.L)
+            headIndex--;
+        else if (output.direction == Direction.R)
+            headIndex++;
+        if (headIndex >= tape.size())
+            tape.addLast(blank); // add a blank symbol
+        else if (headIndex < 0) {
+            tape.addFirst(blank); // add a blank symbol
+            startIndex++;
+            headIndex = 0;
+        }
+        return true;
+    }
+
+    public void createFromFile(String filename) {
+        // create a TM from file (.tm)
+        TMParser tmParser = new TMParser(filename);
+        // state set
+        for (String state : tmParser.getStates())
+            addState(state);
+        // input symbol set
+        for (String symbol : tmParser.getInputSymbols())
+            addInputSymbol(symbol);
+        // tape symbol set
+        for (String symbol : tmParser.getTapeSymbols())
+            addTapeSymbol(symbol);
+        // initial state
+        setInitialState(tmParser.getInitialState());
+        // final states
+        for (String state : tmParser.getFinalStates())
+            addFinalState(state);
+        // transition function
+        for (String[] function : tmParser.getTransitionFunctions()) {
+            addTransitionFunction(function[0], function[1], function[2], function[3], function[4]);
+        }
+    }
+
+    public void run(String input) {
+        boolean illegal = false;
+        System.out.println("Input: " + input);
+        String delimOne = "====================";
+        String delimTwo = "---------------------------------------------";
+        // whether this input is illegal or not
+        char[] inputArray = input.toCharArray();
+        for (char c : inputArray) {
+            if (!inputSymbolSet.contains(String.valueOf(c))) {
+                System.out.println(delimOne + " ERR " + delimOne);
+                System.out.println("The input \"" + input + "\" is illegal");
+                illegal = true;
+                System.out.println(delimOne + " END " + delimOne);
+            }
+        }
+        if (!illegal) {
+            // we now know the input is legal
+            currentState = initialState;
+            for (char c : inputArray)
+                tape.add(String.valueOf(c));
+            step = 0;
+            headIndex = startIndex = 0;
+            System.out.println(delimOne + "RUN" + delimOne);
+            printID();
+            System.out.println(delimTwo);
+            while (oneMove()) {
+                printID();
+                System.out.println(delimTwo);
+            }
+            System.out.print("Result: ");
+            for (String symbol : tape)
+                if (symbol.compareTo(blank) != 0)
+                    System.out.print(symbol);
+            System.out.println();
+            System.out.println(delimOne + "END" + delimOne);
+        }
+    }
+
+    public static void main(String[] args) {
+        TM tm = new TM();
+        tm.createFromFile("./TM/palindrome_detector.tm");
+        tm.run("1111000000001111");
     }
 }
--- src/TMParser.java
+++ src/TMParser.java
@@ -2,11 +2,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TMParser {
-    private String[] TMDescriptor;
+    private static final String parseString = "[ {},=]";
+    private ArrayList<String> TMDescriptor;
 
     TMParser(String fileName) {
+        TMDescriptor = new ArrayList<>();
         /* use a .tm file to initial parser */
         File file = new File(fileName);
         try {
@@ -14,7 +17,10 @@ public class TMParser {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.compareTo("") != 0 && !line.startsWith(";")) {
-                    System.out.println(line);
+                    String[] parseComment = line.split(";");
+                    line = parseComment[0];
+                    TMDescriptor.add(line);
+                    //System.out.println(line);
                 }
             }
             bufferedReader.close();
@@ -23,7 +29,127 @@ public class TMParser {
         }
     }
 
+    private Iterable<String> removeEmpty(String[] parsedElement) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : parsedElement) {
+            if (s.compareTo("") != 0)
+                result.add(s);
+        }
+        return result;
+    }
+
+    public Iterable<String> getStates() {
+        // return all states
+        String statesRaw = null;
+        for (String s : TMDescriptor) {
+            if (s.length() >= 2 && s.charAt(0) == '#' && s.charAt(1) == 'Q') {
+                statesRaw = s.substring(2);
+            }
+        }
+        if (statesRaw == null) {
+            System.out.println("Parsing error! State set not found!");
+            System.exit(0);
+        }
+        String[] parsedStates = statesRaw.split(parseString);
+        return removeEmpty(parsedStates);
+    }
+
+    public Iterable<String> getInputSymbols() {
+        // return all input symbols
+        String inputSymbolsRaw = null;
+        for (String s : TMDescriptor) {
+            if (s.length() > 2 && s.charAt(0) == '#' && s.charAt(1) == 'S') {
+                inputSymbolsRaw = s.substring(2);
+            }
+        }
+        if (inputSymbolsRaw == null) {
+            System.out.println("Parsing error! Input symbol set not found!");
+            System.exit(0);
+        }
+        String[] parsedInputSymbols = inputSymbolsRaw.split(parseString);
+        return removeEmpty(parsedInputSymbols);
+    }
+
+    public Iterable<String> getTapeSymbols() {
+        // return all tape symbols
+        String tapeSymbolsRaw = null;
+        for (String s : TMDescriptor) {
+            if (s.length() > 2 && s.charAt(0) == '#' && s.charAt(1) == 'T') {
+                tapeSymbolsRaw = s.substring(2);
+            }
+        }
+        if (tapeSymbolsRaw == null) {
+            System.out.println("Parsing error! Tape symbol set not found!");
+            System.exit(0);
+        }
+        String[] parsedTapeSymbols = tapeSymbolsRaw.split(parseString);
+        return removeEmpty(parsedTapeSymbols);
+    }
+
+    public String getInitialState() {
+        // return the initial state
+        String initialStateRaw = null;
+        for (String s : TMDescriptor) {
+            if (s.length() > 3 && s.charAt(0) == '#' && s.charAt(1) == 'q' && s.charAt(2) == '0') {
+                initialStateRaw = s.substring(3);
+            }
+        }
+        if (initialStateRaw == null) {
+            System.out.println("Parsing error! Initial state not found!");
+            System.exit(0);
+        }
+        String[] parsedInitialState = initialStateRaw.split(parseString);
+        String initialState = "";
+        for (String s : parsedInitialState) {
+            if (s.compareTo("") != 0)
+                initialState = s;
+        }
+        return initialState;
+    }
+
+    public Iterable<String> getFinalStates() {
+        // return all final states
+        String finalStatesRaw = null;
+        for (String s : TMDescriptor) {
+            if (s.length() > 2 && s.charAt(0) == '#' && s.charAt(1) == 'F') {
+                finalStatesRaw = s.substring(2);
+            }
+        }
+        if (finalStatesRaw == null) {
+            System.out.println("Parsing error! Final state set not found!");
+            System.exit(0);
+        }
+        String[] parsedFinalStates = finalStatesRaw.split(parseString);
+        return removeEmpty(parsedFinalStates);
+    }
+
+    public Iterable<String[]> getTransitionFunctions() {
+        // return all transition functions (in the form of an array consisting of 5 elements)
+        ArrayList<String[]> functions = new ArrayList<>();
+        String[] function;
+        for (String s : TMDescriptor) {
+            if (s.charAt(0) != '#') {
+                function = s.split(" ");
+                if (function.length != 5) {
+                    System.out.println("Parsing error! Transition function not complete!");
+                    System.exit(0);
+                }
+                functions.add(function);
+            }
+        }
+        return functions;
+    }
+
+    // unit test
     public static void main(String[] args) {
         TMParser tmParser = new TMParser("./TM/palindrome_detector.tm");
+        //for (String s : tmParser.getFinalStates())
+        //System.out.println(s);
+        // System.out.println(tmParser.getInitialState());
+        for (String[] test : tmParser.getTransitionFunctions()) {
+            for (String s : test)
+                System.out.print(s + " ");
+            System.out.println();
+        }
     }
 }
